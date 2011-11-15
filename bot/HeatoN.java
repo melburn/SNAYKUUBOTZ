3b7c4b8999ceb94269831869369a8070f4db6f85
unknown <Max@Maxxad-Data.(none)>
2011-11-15 14:24:05 +0100

    max ofardiga skit

--- bot/HeatoN.java
+++ bot/HeatoN.java
@@ -4,6 +4,8 @@ import gameLogic.*;
 
 public class HeatoN implements Brain
 {
+	Snake self;
+	GameState gamestate;
 	public HeatoN()
 	{
 		System.out.print("LOsdadsadL");
@@ -15,10 +17,52 @@ public class HeatoN implements Brain
 		this.gamestate = gamestate;
 		
 		Direction previousDirection = self.getCurrentDirection();
-		if (gamestate.willCollide(self, previousDirection))
+		
+		if (!okToGo(previousDirection))// inte okej att gå rakt
 		{
-			return previousDirection.turnLeft();
+			// behöver alternativ 
+			if(okToGo(previousDirection.turnLeft()))
+			{
+				return previousDirection.turnLeft();
+			}
+			
+			if(okToGo(previousDirection.turnRight()))
+			{
+				return previousDirection.turnRight();
+			}
 		}
-		return previousDirection;
+		return previousDirection; // gå rakt eller inget alt
 	}
+	
+	private boolean okToGo(Direction direction)
+	{
+		Position toCheck = direction.calculateNextPosition(self.getHeadPosition());
+		Board board = gamestate.getBoard();
+		
+		// kommer collida dir		
+		if (gamestate.willCollide(self, direction))
+		{
+			return false;	
+		}
+		
+		// om det kan vara en trap		
+		Position left =  direction.turnLeft().calculateNextPosition(self.getHeadPosition());
+		Position right =  direction.turnRight().calculateNextPosition(self.getHeadPosition());
+		
+		if(board.isLethal(left) && board.isLethal(right))
+		{
+			// might be trap
+			return doTheWalk(direction);
+		}
+		
+		return true;
+		
+	}
+	
+	public boolean doTheWalk(Direction direction)
+	{
+		return false;
+	}
+	
+	
 }
