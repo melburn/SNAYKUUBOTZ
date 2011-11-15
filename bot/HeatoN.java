e894d1753c8152739c05087130760ae1289b2b61
Melburn <Melburn1@hotmail.com>
2011-11-15 13:06:43 +0100

    lolcommit

--- bot/HeatoN.java
+++ bot/HeatoN.java
@@ -4,8 +4,22 @@ import gameLogic.*;
 
 public class HeatoN implements Brain
 {
+	private GameState gamestate;
+	private Snake self;
 	public HeatoN()
 	{
 		System.out.print("LOsdadL");
+	}
+	public Direction getNextMove(Snake yourSnake, GameState gamestate)
+	{
+		self = yourSnake;
+		this.gamestate = gamestate;
+		
+		Direction previousDirection = self.getCurrentDirection();
+		if (gamestate.willCollide(self, previousDirection))
+		{
+			return previousDirection.turnLeft();
+		}
+		return previousDirection;
 	}
 }
