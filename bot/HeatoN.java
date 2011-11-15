668752a21ff0871158032455bba7ebf7a0fedf51
unknown <Max@Maxxad-Data.(none)>
2011-11-15 13:04:34 +0100

    allt

--- bot/HeatoN.java
+++ bot/HeatoN.java
@@ -6,7 +6,7 @@ public class HeatoN implements Brain
 {
 	public HeatoN()
 	{
-		System.out.print("LOsdadL");
+		System.out.print("LOsdadsadL");
 	}
 	
 	public Direction getNextMove(Snake yourSnake, GameState gamestate)
