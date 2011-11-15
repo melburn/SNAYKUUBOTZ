9ec9a310ab7a40dd149b7b38e9fef1b33e00b62c
Melburn <Melburn1@hotmail.com>
2011-11-15 14:22:48 +0100

    jag har fixat en hitta nrmsta frukt

--- bot/HeatoN.java
+++ bot/HeatoN.java
@@ -1,10 +1,13 @@
 package bot;
 
 import gameLogic.*;
+import java.util.*;
+
 
 public class HeatoN implements Brain
 {
 	private GameState gamestate;
+	private Board currBoard;
 	private Snake self;
 	public HeatoN()
 	{
@@ -14,13 +17,48 @@ public class HeatoN implements Brain
 	{
 		self = yourSnake;
 		this.gamestate = gamestate;
+		currBoard = gamestate.getBoard();
+		int range = 10;
+		
+		Position fruitPosition = getClosestFruit();
+		if(fruitPosition != null && gamestate.distanceBetween(fruitPosition, self.getHeadPosition()) < range)
+		{
+			//TODO pursuit froot
+		}
 		
 		Direction previousDirection = self.getCurrentDirection();
 		if (gamestate.willCollide(self, previousDirection))
 		{
+			if(gamestate.willCollide(self, previousDirection.turnLeft()))
+				return previousDirection.turnRight();
 			return previousDirection.turnLeft();
 		}
 		return previousDirection;
 	}
+	
+	private Position getClosestFruit()
+	{
+		ArrayList<Position> fruitList = gamestate.getFruits();
+		if (fruitList == null)
+			return null;
+		Position closestFruit = null;
+		int shortestDistance = 10000000;
+		for (Position p : fruitList)
+		{
+			int fruitDistance = gamestate.distanceBetween(self.getHeadPosition(), closestFruit);
+			if (closestFruit != null)
+			{
+				closestFruit = p;
+				shortestDistance = fruitDistance;
+			}
+			if (fruitDistance < shortestDistance)
+			{
+				closestFruit = p;
+				shortestDistance = fruitDistance;
+			}
+		}
+		return closestFruit;
+	}
+	
 }
 
